6fb39a961f0a421e22808028ec1ebfb532a69087
PABLO MARTÍNEZ GÁLVEZ <pmg943@inlumine.ual.es>
2026-05-19 02:20:50 +0200

    refactor: restructure backend package to com.vibesevents

--- backend/src/main/java/com/vibesevents/VibesEventsApplication.java
+++ backend/src/main/java/com/vibesevents/VibesEventsApplication.java
@@ -1,13 +1,13 @@
-package com.vibesevents.backend;
+package com.vibesevents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class VibesEventsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(VibesEventsApplication.class, args);
 	}
 
 }
